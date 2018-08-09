bd98ebf3ecb636e7001959f37a95c741f86bb585
MySticHorYzoN <fastor26@gmail.com>
2018-08-10 00:55:39 +0200

    Add files via upload

---
+++ src/NoDropItems/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NoDropItems;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerDropItemEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->getServer()->getLogger->info("§eNoDropItems Activated!");
+
+}
+
+public function onDropItem(PlayerDropItemEvent $event){
+
+$event->setCancelled(true);
+
+}
+
+}
